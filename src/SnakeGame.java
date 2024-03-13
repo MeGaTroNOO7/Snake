45dae85b4204d9ea3d683f7cb38d132998adbb1d
bernard_george_charles <bernardgeorgecharles007@gmail.com>
2024-03-13 13:11:40 +0530

    init

---
+++ src/SnakeGame.java
@@ -0,0 +1,11 @@
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class SnakeGame {
+
+    public static void main(String[] args) {
+
+        new GameFrame();
+
+    }
+}
